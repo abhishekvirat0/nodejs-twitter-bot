fef4dca4dc0597e4ec34448abc08c9306d794e3a
abhishek <31734493+abhishekvirat0@users.noreply.github.com>
2019-06-12 21:38:42 +0530

    added config

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 module.exports = {
-  consumer_key:         'rj1hNa9XeZoBqy01OPjRCnvcU',
-  consumer_secret:      'aZty21qrrzO0GjTEJG0JkkA2vNT6BEr8ad9lwTc2iwcL2w7FEi',
-  access_token:         '893136021638041600-FbTjnd0RBiaIOQo6y8VlrHYWYTgLzhd',
-  access_token_secret:  'rK6YSaHGwK1GYJ9YZIwTl6r7KVdP0M9CeeOn660pdZlhW',
+  consumer_key:         '********GO TO TWITTER DEVELOPER PAGE************',
+  consumer_secret:      '************************************************',
+  access_token:         '***************************************',
+  access_token_secret:  '******************************',
   timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
   strictSSL:            true,     // optional - requires SSL certificates to be valid.
 };
